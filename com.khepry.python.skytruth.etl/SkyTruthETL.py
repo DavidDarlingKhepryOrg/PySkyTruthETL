f59346a9e8e734f138ec46ab7ea8e09801177198
KhepryQuixote <KhepryQuixote@localhost>
2014-08-28 19:21:52 -0500

    Incremental commit of PySkyTruthETL project:     + added functionality to output SQLite database from DataFrames     + added functionality to output MySQL database from DataFrames     + commented out MySQL functionality due to "Broken Pipe" issue

--- com.khepry.python.skytruth.etl/SkyTruthETL.py
+++ com.khepry.python.skytruth.etl/SkyTruthETL.py
@@ -46,8 +46,11 @@
 
 import codecs
 import gc
+import mysql.connector
 import os
 import pandas
+# import pyodbc # commented out because not yet available in Python 3
+import sqlite3
 import sys
 
 # This path is for the source data files
@@ -56,6 +59,11 @@ dataFilePath = '~/Documents/fracking/SkyTruth'
 # This path is for the target data files
 trgtFldrPath = '~/Documents/fracking/tempFldr'
 trgtFileDelim = ','
+trgtBlndFile = 'blndFile.txt'
+trgtChemFile = 'chemFile.txt'
+trgtRprtFile = 'rprtFile.txt'
+trgtSqliteDb = 'FracFocusXtract.db'
+trgtDatabase = ''
 
 # These file names are the various "Report" file names
 rprtFileNames = ['2012_FracFocusReport.txt', '2013_FracFocusReport.txt']
@@ -64,6 +72,14 @@ rprtFileDelim = '\t'
 # These file names are the various "Chemical" file names
 chemFileNames = ['2012_FracFocusChemical.txt', '2013_FracFocusChemical.txt']
 chemFileDelim = '\t'
+
+# MySQL database connection values
+sqlDriver = 'MySQL ODBC 5.3 Unicode Driver'
+sqlHost = 'localhost'
+sqlPort = '3306'
+sqlDB = 'FracFocusXtract'
+sqlUID = 'root'
+sqlPWD = 'Radical1'
 
 # ============================================================================
 # Mainline Processing Routine
@@ -102,15 +118,18 @@ def main():
     # so that if the program abends, the end-user won't
     # get the impression that the program was successful
     # based upon the presence of all of the expected files.
-    rprtFileExpanded = os.path.join(trgtFldrPathExpanded, "rprtFile.txt")
+    rprtFileExpanded = os.path.join(trgtFldrPathExpanded, trgtRprtFile)
     if os.path.exists(rprtFileExpanded):
         os.remove(rprtFileExpanded)
-    chemFileExpanded = os.path.join(trgtFldrPathExpanded, "chemFile.txt")
+    chemFileExpanded = os.path.join(trgtFldrPathExpanded, trgtChemFile)
     if os.path.exists(chemFileExpanded):
         os.remove(chemFileExpanded)
-    blndFileExpanded = os.path.join(trgtFldrPathExpanded, "blndFile.txt")
+    blndFileExpanded = os.path.join(trgtFldrPathExpanded, trgtBlndFile)
     if os.path.exists(blndFileExpanded):
         os.remove(blndFileExpanded)
+    sqliteDbExpanded = os.path.join(trgtFldrPathExpanded, trgtSqliteDb)
+    if os.path.exists(sqliteDbExpanded):
+        os.remove(sqliteDbExpanded)
         
     # Append the report file(s) into one report file
     for fileName in rprtFileNames:
@@ -120,6 +139,7 @@ def main():
             sys.stderr.write("Report file does NOT exist: %s%s" % (fileNameExpanded, os.linesep))
             return
         dataFrame = pandas.read_csv(fileNameExpanded, sep=rprtFileDelim, error_bad_lines=False)
+        dataFrame = dataFrame.fillna(' ')
         with codecs.open(rprtFileExpanded, 'a') as f:
             dataFrame.to_csv(f, index=False)
         # clean up memory
@@ -134,6 +154,7 @@ def main():
             sys.stderr.write("Chemical file does NOT exist: %s%s" % (fileNameExpanded, os.linesep))
             return
         dataFrame = pandas.read_csv(fileNameExpanded, sep=chemFileDelim, error_bad_lines=False)
+        dataFrame = dataFrame.fillna(' ')
         with codecs.open(chemFileExpanded, 'a') as f:
             dataFrame.to_csv(f, index=False)
         # clean up memory
@@ -160,12 +181,55 @@ def main():
     # and output the resulting "blended" file
     print ("Blending the report rows with the chemical rows")
     blndDataFrame = pandas.merge(rprtDataFrame, chemDataFrame, on='pdf_seqid', how='inner', suffixes=('_rpt','_chm'))
+    blndDataFrame.fillna(' ')
     with codecs.open(blndFileExpanded, 'wb', 'cp1252') as f:
         blndDataFrame.to_csv(f, index=False)
         # Uncomment the "pandas.merge" statement below if you want to pull in the data,
         # blend it, and push it out to the "blended" file without creating an "blended" DataFrame in between.
         # If you do so, remember to comment out the "blndDataFrame" statements above and below as they're aren't needed.
         # pandas.merge(rprtDataFrame, chemDataFrame, on='pdf_seqid', how='inner', suffixes=('_rpt','_chm')).to_csv(f, index=False)
+
+    # Output the resulting DataFrames
+    # to the specified database/tables
+
+    print ("Output the results to an SQLite database")
+    
+    # cnx = sqlite3.connect(':memory:')
+    cnx = sqlite3.connect(sqliteDbExpanded)
+   
+    rprtDataFrame.to_sql('reports', cnx, flavor='sqlite', if_exists='replace')
+    chemDataFrame.to_sql('chemicals', cnx, flavor='sqlite', if_exists='replace')
+    blndDataFrame.to_sql('reports_chemicals', cnx, flavor='sqlite', if_exists='replace')
+
+    cnx.close()
+    del cnx
+    
+    print ("Establish a connection to the MySQL database")        
+
+    modality = None
+    try:
+        # attempt connection with ODBC
+        sqlConnString = "Driver={%s};Server=%s;Port=%s;UID=%s;Database=%s;" % (sqlDriver, sqlHost, sqlPort, sqlUID, sqlDB)
+        print ("pyODBC srcConnString: %s" % sqlConnString)
+        sqlConnString += "PWD=%s;" % sqlPWD
+        sqlConnection = pyodbc.connect(sqlConnString)
+        modality = "pyodbc"
+    except:
+        # attempt connection with MySQL connector
+        sqlConnParms = {'user':sqlUID, 'host':sqlHost, 'port':sqlPort, 'database':sqlDB}
+        print ("MySQL connString: %s" % sqlConnParms)
+        sqlConnParms['password'] = sqlPWD
+        sqlConnection = mysql.connector.connect(**sqlConnParms)
+        modality = "mysql.connector"
+
+#    print ("Output the results to MySQL database")        
+#
+#    rprtDataFrame.to_sql('reports', sqlConnection, flavor='mysql', if_exists='replace')
+#    chemDataFrame.to_sql('chemicals', sqlConnection, flavor='mysql', if_exists='replace')
+#    blndDataFrame.to_sql('reports_chemicals', sqlConnection, flavor='mysql', if_exists='replace')
+    
+    sqlConnection.close()
+    del sqlConnection
         
     # Clean up memory
     del blndDataFrame
